c55d53bf59ec858f28e4a47ee23c6007cea9b414
sohit-vashishtha <sohitvashishtha8595@gmail.com>
2025-12-25 22:08:49 +0530

    first commit

---
+++ Task1.py
@@ -0,0 +1,14 @@
+#Take two numbers as input from the user and print their sum and difference.
+num1=input("Enter the first number: ")
+num2=input("Enter the second number: ")
+
+function_sum=int(num1)+int(num2)
+function_diff=int(num1)-int(num2)
+function_multiply=int(num1)*int(num2)
+function_divide=int(num1)/int(num2)
+
+print("Addition of the two numbers is:",function_sum)
+print("Subtraction of the two numbers is:",function_diff)
+print("Multiplication of the two numbers is:",function_multiply)
+print("Division of the two numbers is:",function_divide)
+
---
+++ Task2.py
@@ -0,0 +1,7 @@
+#Create a Personalized Greeting
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("Hello " + firstName + " " + lastName + ", welcome to our website!")
+
